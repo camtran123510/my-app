eeef7789ed55c45947c188d6408a7826622b586a
Cam Tran <camtran123510@gmail.com>
2022-02-28 19:18:06 -0800

    Updates

---
+++ precache-manifest.454ec4f4d163234ea68860114b136ee7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "15b6e6e79c2e35f68cfe",
+    "url": "/my-app/static/css/main.f3f20d44.chunk.css"
+  },
+  {
+    "revision": "15b6e6e79c2e35f68cfe",
+    "url": "/my-app/static/js/main.15b6e6e7.chunk.js"
+  },
+  {
+    "revision": "7d8a5b69bced8be18895",
+    "url": "/my-app/static/js/1.7d8a5b69.chunk.js"
+  },
+  {
+    "revision": "7d45dcf35bcbed4a19d6",
+    "url": "/my-app/static/js/2.7d45dcf3.chunk.js"
+  },
+  {
+    "revision": "89cfe04884a91f8d06b6",
+    "url": "/my-app/static/js/runtime~main.89cfe048.js"
+  },
+  {
+    "revision": "03402c3f6921eb49d2d6962dc3c246bf",
+    "url": "/my-app/index.html"
+  }
+];
